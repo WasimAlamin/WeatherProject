8c461a9ef549ccb8c558059c39002299ec14044a
Wasem Alamin Hassan <waal0004@student.hv.se>
2021-12-08 03:29:23 +0100

    Add project files.

---
+++ WeatherProject/WeatherProject/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherProject.Models
+{
+   public static class Constants
+    {
+
+        public static string WeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string APIKey = "748ed854eaf2ed29aaa5409e085c3635";
+      
+    }
+}
---
+++ WeatherProject/WeatherProject/View/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherProject.Models;
+using WeatherProject.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+        }
+
+        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+            {
+                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.WeatherEndpoint));
+                BindingContext = weatherData;
+            }
+        }
+
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={_cityEntry.Text}";
+            requestUri += "&units=metric";
+            requestUri += $"&APPID={Constants.APIKey}";
+            return requestUri;
+        }
+    }
+}
